71ae25037ba3e7159c7251de56ddc68ff710f4de
anna-khu <annakhulup@gmail.com>
2024-06-25 11:24:14 -0700

    Initial Commit

---
+++ prac/pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  pracApp.swift
+//  prac
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
